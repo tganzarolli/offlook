ff7a8b1970a509121b0a8ec3397a7c7271e28e6e
Thiago Ganzarolli <tganzar@vmcommerce.intra>
2015-07-09 02:47:38 -0300

    final command

--- bin/install.sh
+++ bin/install.sh
@@ -9,3 +9,7 @@ echo "Setting up Google Calendar Id"
 ./syncher.rb -c
 echo "Setting up Google credentials and performing initial full synch. This may take a while"
 ./syncher.rb -fv
+echo "Schedulling partial synch to run every hour"
+cp ./goodies/com.zerowidth.launched.offlook.plist ~/Library/LaunchAgents/
+launchctl load -w ~/Library/LaunchAgents/com.zerowidth.launched.offlook.plist
+echo "All set. Enjoy!"
