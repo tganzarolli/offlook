c98dceb2ea8baa65c5177312d03f87868ecf97c8
Thiago Ganzarolli <tganzar@vmcommerce.intra>
2015-07-09 15:08:22 -0300

    fixing sed substitution

--- bin/install.sh
+++ bin/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash --login
+#!/bin/bash --login -e
 echo "Installing ruby gems"
 rvm gemset use offlook --create
 gem install bundler --no-ri --no-rdoc
@@ -10,7 +10,7 @@ echo "Setting up Google Calendar Id"
 echo "Setting up Google credentials and performing initial full synch. This may take a while"
 ./syncher.rb -fv
 echo "Schedulling partial synch to run every hour"
-dir=$`pwd`
+dir=`pwd`
 sed -e s/FILL_WITH_YOUR_USER/`whoami`/g goodies/com.zerowidth.launched.offlook.plist | sed -e  "s/FILL_WITH_YOUR_PATH/${dir//\//\\/}/g" > /tmp/com.zerowidth.launched.offlook.plist
 mv /tmp/com.zerowidth.launched.offlook.plist ~/Library/LaunchAgents/com.zerowidth.launched.offlook.plist
 launchctl load -w ~/Library/LaunchAgents/com.zerowidth.launched.offlook.plist
